cfdf1c5951718613c7c11ce221f9e8db4a3e3ce1
charo7507 <charolite.technologie@gmail.com>
2022-10-06 20:44:35 +0200

    initialisation

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    n
+  </div>
+</template>
+
+<script setup>
+
+</script>
